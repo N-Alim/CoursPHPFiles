c31d3a6739bc951e8a0188325e42cd57f65921fb
Nouira Alim <nouiraalim@gmail.com>
2022-02-14 16:31:09 +0100

    Modification de write
    Retrait de l'ajout à la fin du fichier

--- classes/Log.php
+++ classes/Log.php
@@ -4,6 +4,6 @@ class Log
 {
     public static function write($error)
     {
-        file_put_contents("./logs/erreur.txt", "Error", FILE_APPEND);
+        file_put_contents("./logs/erreur.txt", "Error");
     }
 }
