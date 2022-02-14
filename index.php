6f105ac4453790686032a425d6c2585681c409f8
Nouira Alim <nouiraalim@gmail.com>
2022-02-14 16:31:48 +0100

    Test de write() de la classe Log

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ $contenu = file_get_contents("./files/toto.txt");
 $contenu .= "\nLa suite du fichier";
 file_put_contents("./files/toto.txt", $contenu);
 
+Log::write("Erreur");
+
 // fclose($handle);
 
 // var_dump($contents);
